edae4135a87deb867c93b546778bfb17b0a08798
blhwong <b.lh.wong@gmail.com>
2017-02-20 18:43:27 -0800

    Pass all test cases in SongQueue

--- client/collections/SongQueue.js
+++ client/collections/SongQueue.js
@@ -10,20 +10,30 @@ var SongQueue = Backbone.Collection.extend({
     // this.listenTo(this, 'add', this.playFirst);
     // debugger;
     this.listenTo(this, 'add', function() {
-      debugger;
+      // debugger;
       if (this.length < 2) {
         this.playFirst();
       }
     });
     this.listenTo(this, 'ended', function() {
+      // debugger;
       this.shift();
       if (this.length > 0) {
         this.playFirst();
       }
     });
+    this.listenTo(this, 'dequeue', function() {
+      // debugger;
+      this.shift();
+    });
   },
   playFirst: function() {
+    // model.play();
     debugger;
-  }
+    this.models[0].play();
+  },
+  // remove: function(target) {
+  //   debugger;
+  // }
 
 });
