061f40e63e2733028dcfd0c5b24d1d02de9d05b2
blhwong <b.lh.wong@gmail.com>
2017-02-20 17:43:54 -0800

    Pass song added tests

--- client/collections/SongQueue.js
+++ client/collections/SongQueue.js
@@ -1,9 +1,23 @@
 // SongQueue.js - Defines a backbone model class for the song queue.
+// ships.add([
+//   {name: "Flying Dutchman"},
+//   {name: "Black Pearl"}
+// ]);
 var SongQueue = Backbone.Collection.extend({
 
   model: SongModel,
-
   initialize: function() {
+    // this.listenTo(this, 'add', this.playFirst);
+    // debugger;
+    this.listenTo(this, 'add', function() {
+      debugger;
+      if (this.length < 2) {
+        this.playFirst();
+      }
+    });
+  },
+  playFirst: function() {
+    debugger;
   }
 
 });
