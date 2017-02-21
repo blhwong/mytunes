b0696503765842ad8591209208cd2ed7754b72d2
blhwong <b.lh.wong@gmail.com>
2017-02-20 20:23:11 -0800

    Improve to playerView

--- client/views/PlayerView.js
+++ client/views/PlayerView.js
@@ -4,8 +4,13 @@ var PlayerView = Backbone.View.extend({
   // HTML5 (native) audio tag is being used
   // see: https://developer.mozilla.org/en-US/docs/Web/Guide/HTML/Using_HTML5_audio_and_video
   el: '<audio controls autoplay />',
-
+  // $(appView.playerView.el).trigger('ended')
   initialize: function() {
+    debugger;
+    this.listenTo(this.model, 'ended', function() {
+      // this.appView.playerView.model;
+      debugger;
+    });
   },
 
   setSong: function(song) {
